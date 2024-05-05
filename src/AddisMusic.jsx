3d81fe7721e24679575b613ad07d666cbef23c5a
sgc93 <smachewgedefaw@gmail.com>
2024-05-05 10:53:35 -0900

    defining initial routes

--- src/AddisMusic.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-	return (
-		<div>
-			<h1>Well come to addismusic</h1>
-		</div>
-	);
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <HomePage />,
+	},
+	{
+		path: "auth",
+		element: <AuthPage />,
+	},
+]);
+
+function App() {
+	return (
+		<RouterProvider router={router}>
+			<Outlet />
+		</RouterProvider>
+	);
+}
+
+export default App;
