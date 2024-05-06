1e6d914f95301b5053a3b2d12e5b6433fae87b94
sgc93 <smachewgedefaw@gmail.com>
2024-05-05 21:58:51 -0900

    configure redux and redux-saga middleware

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { configureStore } from "@reduxjs/toolkit";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import createSagaMiddleware from "redux-saga";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const saga = createSagaMiddleware();
+
+const store = configureStore({
+	reducer: {},
+	middleware: [saga],
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>
+);
