044971961394d6d81a181978a098edb57b3ecfc1
sgc93 <smachewgedefaw@gmail.com>
2024-05-07 09:23:26 -0900

    update home page bg

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,17 @@
-import AuthPage from "./AuthPage";
+import styled from "@emotion/styled";
+import SideBar from "../features/sidebar/SideBar";
+
+const HomeSection = styled.section`
+	width: 100vw;
+	height: 100vh;
+	background: radial-gradient(var(--color-rad-center), var(--color-rad-outer));
+`;
 
 const HomePage = () => {
 	return (
-		<div>
-			<h1>Welcome to addis music</h1>
-			<AuthPage />
-		</div>
+		<HomeSection>
+			<SideBar />
+		</HomeSection>
 	);
 };
 
