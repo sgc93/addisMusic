0cdf9f8df46104bdf0b88dfcd41a2ab6b65cd556
sgc93 <smachewgedefaw@gmail.com>
2024-05-07 10:26:56 -0900

    add tabs

--- src/features/sidebar/SideBar.jsx
+++ src/features/sidebar/SideBar.jsx
@@ -1,15 +1,59 @@
 import styled from "@emotion/styled";
+import { MdAlbum, MdHome, MdPerson4, MdQueueMusic } from "react-icons/md";
 import Logo from "../../ui/Logo";
+import SideBarTab from "./SideBarTab";
 
 const Sidebar = styled.div`
-	width: 15vw;
-	padding: 0.3rem;
+	width: 16vw;
+	padding: 0.6rem;
+`;
+
+const SidebarChild = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 0.5rem;
+	width: 100%;
+
+	padding: 1rem 0rem 1rem 1rem;
+	background-color: var(--color-bg-secondary);
+
+	border-top-left-radius: 1rem;
+`;
+
+const IconStyle = styled.div`
+	font-size: 26px;
 `;
 
 const SideBar = () => {
 	return (
 		<Sidebar>
-			<Logo />
+			<SidebarChild>
+				<Logo />
+				<SideBarTab handleClick={() => console.log("go to home")}>
+					<IconStyle>
+						<MdHome />
+					</IconStyle>
+					Home
+				</SideBarTab>
+				<SideBarTab handleClick={() => console.log("go to albums")}>
+					<IconStyle>
+						<MdAlbum />
+					</IconStyle>
+					Top Albums
+				</SideBarTab>
+				<SideBarTab handleClick={() => console.log("go to artists")}>
+					<IconStyle>
+						<MdPerson4 />
+					</IconStyle>
+					Top Artists
+				</SideBarTab>
+				<SideBarTab handleClick={() => console.log("go to tracks")}>
+					<IconStyle>
+						<MdQueueMusic />
+					</IconStyle>
+					Top Tracks
+				</SideBarTab>
+			</SidebarChild>
 		</Sidebar>
 	);
 };
