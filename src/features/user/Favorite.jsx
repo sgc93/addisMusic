984b15fac163dab35ed71d687222e88deaef24e0
sgc93 <smachewgedefaw@gmail.com>
2024-05-08 05:58:56 -0900

    create component to list down user's favorites

---
+++ src/features/user/Favorite.jsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+const FavoriteBox = styled.div`
+	padding: 0.5rem 1rem;
+`;
+
+const Favorite = () => {
+	return <FavoriteBox>User Favorite</FavoriteBox>;
+};
+
+export default Favorite;
