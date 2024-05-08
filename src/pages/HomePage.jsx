3f165e7cdde99ec531d60e8d06bcfd52453c2fcb
sgc93 <smachewgedefaw@gmail.com>
2024-05-07 16:25:21 -0900

    create specific area for sub-route sections

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { Outlet } from "react-router-dom";
 import Footer from "../features/footer/Footer";
 import Header from "../features/header/Header";
 import SideBar from "../features/sidebar/SideBar";
@@ -9,12 +10,28 @@ const HomeSection = styled.section`
 	background: radial-gradient(var(--color-rad-center), var(--color-rad-outer));
 `;
 
+const OutletSection = styled.section`
+	position: absolute;
+	top: 9%;
+	right: 0;
+
+	width: 80.5%;
+	height: 81%;
+
+	margin: 0rem 0.8rem;
+	background-color: var(--color-bg-secondary);
+	border-radius: 1rem;
+`;
+
 const HomePage = () => {
 	return (
 		<HomeSection>
 			<SideBar />
 			<Header />
 			<Footer />
+			<OutletSection>
+				<Outlet />
+			</OutletSection>
 		</HomeSection>
 	);
 };
