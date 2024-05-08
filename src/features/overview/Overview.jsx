94b88ab7b82f300d0578f3074fcf32cda0c560e9
sgc93 <smachewgedefaw@gmail.com>
2024-05-07 16:28:10 -0900

    create component to display homve overview

---
+++ src/features/overview/Overview.jsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+const OverviewBox = styled.div`
+	padding: 0.5rem 1rem;
+`;
+
+const Overview = () => {
+	return <OverviewBox>Home Overview</OverviewBox>;
+};
+
+export default Overview;
