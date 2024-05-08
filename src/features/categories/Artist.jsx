fdf205c89d1e94cd37730491122541c19fc523e4
sgc93 <smachewgedefaw@gmail.com>
2024-05-07 16:26:21 -0900

    create component to list down top artists

---
+++ src/features/categories/Artist.jsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+const ArtistBox = styled.div`
+	padding: 0.5rem 1rem;
+`;
+
+const Artist = () => {
+	return <ArtistBox>Home Artist</ArtistBox>;
+};
+
+export default Artist;
