c2dda48eb58f22fbe7f035aebb72353cdcd811b2
sgc93 <smachewgedefaw@gmail.com>
2024-05-08 05:58:28 -0900

    update scrollable side bar menu

--- src/features/sidebar/SideBarMenu.jsx
+++ src/features/sidebar/SideBarMenu.jsx
@@ -2,25 +2,15 @@ import styled from "@emotion/styled";
 import { LuLibrary } from "react-icons/lu";
 import { MdLibraryMusic } from "react-icons/md";
 import { TbMusicHeart, TbMusicPlus } from "react-icons/tb";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useSignedInUser } from "../../hooks/CheckAuth";
+import { checkSignUpOpen } from "../auth/authSlice";
 import SideBarTab from "./SideBarTab";
 
 const Menu = styled.div`
 	display: flex;
 	flex-direction: column;
-
-	overflow-x: hidden;
-	overflow-y: scroll;
-	height: 8.3rem;
-
-	&::-webkit-scrollbar {
-		background-color: transparent;
-		width: 0.6rem;
-	}
-
-	&::-webkit-scrollbar-thumb {
-		background-color: var(--color-bg-secondary);
-		border-radius: 0.6rem;
-	}
 `;
 
 const IconStyle = styled.div`
@@ -35,6 +25,22 @@ const Title = styled.div`
 	color: var(--color-text-primary);
 `;
 
+const MenuItemList = styled.div`
+	overflow-x: hidden;
+	overflow-y: scroll;
+	height: 6.3rem;
+
+	&::-webkit-scrollbar {
+		background-color: transparent;
+		width: 0.6rem;
+	}
+
+	&::-webkit-scrollbar-thumb {
+		background-color: var(--color-bg-secondary);
+		border-radius: 0.6rem;
+	}
+`;
+
 const MenuItem = styled.div`
 	display: flex;
 	flex-direction: column;
@@ -47,6 +53,26 @@ const MenuItem = styled.div`
 const MenuTitle = styled.div``;
 
 const SideBarMenu = ({ handleClick, children, style }) => {
+	const user = useSignedInUser();
+	const dispatch = useDispatch();
+	const navigateTo = useNavigate();
+
+	const isUserSignedIn = () => {
+		if (user) {
+			return true;
+		} else {
+			return false;
+		}
+	};
+
+	const openMenuRoute = (route) => {
+		if (isUserSignedIn()) {
+			navigateTo(`/${route}`);
+		} else {
+			dispatch(checkSignUpOpen());
+		}
+	};
+
 	return (
 		<Menu style={style} onClick={handleClick}>
 			<Title>
@@ -55,35 +81,37 @@ const SideBarMenu = ({ handleClick, children, style }) => {
 				</IconStyle>
 				<span>Your things</span>
 			</Title>
-			<MenuItem>
-				<MenuTitle>Create your own playlist </MenuTitle>
-				<SideBarTab handleClick={() => console.log("go to tracks")}>
-					<IconStyle>
-						<TbMusicPlus />
-					</IconStyle>
-					Create Playlist
-				</SideBarTab>
-			</MenuItem>
-			<MenuItem>
-				<MenuTitle>Have list of selected musics</MenuTitle>
-				<SideBarTab handleClick={() => console.log("go to tracks")}>
-					<IconStyle>
-						<MdLibraryMusic />
-					</IconStyle>
-					Your Songs
-				</SideBarTab>
-			</MenuItem>
-			<MenuItem>
-				<MenuTitle>
-					Add and watch over whenever you like what you have liked
-				</MenuTitle>
-				<SideBarTab handleClick={() => console.log("go to tracks")}>
-					<IconStyle>
-						<TbMusicHeart />
-					</IconStyle>
-					Your Favorites
-				</SideBarTab>
-			</MenuItem>
+			<MenuItemList>
+				<MenuItem>
+					<MenuTitle>Create your own playlist </MenuTitle>
+					<SideBarTab handleClick={() => openMenuRoute("playlists")}>
+						<IconStyle>
+							<TbMusicPlus />
+						</IconStyle>
+						Create Playlist
+					</SideBarTab>
+				</MenuItem>
+				<MenuItem>
+					<MenuTitle>Have list of selected musics</MenuTitle>
+					<SideBarTab handleClick={() => openMenuRoute("songs")}>
+						<IconStyle>
+							<MdLibraryMusic />
+						</IconStyle>
+						Your Songs
+					</SideBarTab>
+				</MenuItem>
+				<MenuItem>
+					<MenuTitle>
+						Add and watch over whenever you like what you have liked
+					</MenuTitle>
+					<SideBarTab handleClick={() => openMenuRoute("favorites")}>
+						<IconStyle>
+							<TbMusicHeart />
+						</IconStyle>
+						Your Favorites
+					</SideBarTab>
+				</MenuItem>
+			</MenuItemList>
 		</Menu>
 	);
 };
