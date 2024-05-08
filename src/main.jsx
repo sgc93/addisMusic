a940f845d47af3c86276a8f68c6cc8bf785f55da
sgc93 <smachewgedefaw@gmail.com>
2024-05-08 07:48:55 -0900

    run parent saga with forked sagas

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
-import authSaga from "./features/auth/authSaga.js";
 import "./index.css";
+import appSaga from "./redux/redux_saga.js";
 import { saga, store } from "./redux/redux_store.js";
 
-saga.run(authSaga);
+saga.run(appSaga);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
