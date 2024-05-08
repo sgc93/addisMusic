bd45a04e133a67469aa14f0ab0ee92d02ba6562f
sgc93 <smachewgedefaw@gmail.com>
2024-05-08 05:59:17 -0900

    create component to list down user's saved songs

---
+++ src/features/user/UserSong.jsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+const UserSongBox = styled.div`
+	padding: 0.5rem 1rem;
+`;
+
+const UserSong = () => {
+	return <UserSongBox>User songs</UserSongBox>;
+};
+
+export default UserSong;
