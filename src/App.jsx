9b852f8312b9b53408cc1e6c18ccbcdb68093bd3
sgc93 <smachewgedefaw@gmail.com>
2024-05-07 16:25:35 -0900

    define routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import Album from "./features/categories/Album";
+import Artist from "./features/categories/Artist";
+import Track from "./features/categories/Track";
+import Overview from "./features/overview/Overview";
 import AuthPage from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
 
@@ -7,6 +11,24 @@ const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <HomePage />,
+		children: [
+			{
+				path: "",
+				element: <Overview />,
+			},
+			{
+				path: "albums",
+				element: <Album />,
+			},
+			{
+				path: "artists",
+				element: <Artist />,
+			},
+			{
+				path: "tracks",
+				element: <Track />,
+			},
+		],
 	},
 	{
 		path: "auth",
