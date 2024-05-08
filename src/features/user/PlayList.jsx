e796b59148d1bcd073853aea5cd4b624f32499e2
sgc93 <smachewgedefaw@gmail.com>
2024-05-08 05:59:07 -0900

    create component to list down user's playlists

---
+++ src/features/user/PlayList.jsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+const PlayListBox = styled.div`
+	padding: 0.5rem 1rem;
+`;
+
+const PlayList = () => {
+	return <PlayListBox>User PlayList</PlayListBox>;
+};
+
+export default PlayList;
