fb5f4b0cd34eac251bf4555530b91392347d06a7
sgc93 <smachewgedefaw@gmail.com>
2024-05-08 10:55:40 -0900

    handle signing up with error handling

--- src/features/auth/signUp/signUpSaga.js
+++ src/features/auth/signUp/signUpSaga.js
@@ -1,21 +1,53 @@
+import { FirebaseError } from "firebase/app";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { put, takeEvery } from "redux-saga/effects";
 import { auth } from "../../../config/firebase_config";
-import { checkSignUpFailure, checkSignUpSuccess } from "./signUpSlice";
+import { isEmailValid, isPasswordValid } from "../../../utils/validators";
 
 function* workCheckSignUP(action) {
 	const { email, password } = action.payload;
-	console.log(`user data: email = ${email} and Password = ${password}`);
 	try {
-		yield createUserWithEmailAndPassword(auth, email, password);
-		put(checkSignUpSuccess());
+		if (!isEmailValid(email) && !isPasswordValid(password)) {
+			yield createUserWithEmailAndPassword(auth, email, password);
+			yield { type: "signUpCheck/checkSignUpSuccess" };
+			yield put({ type: "signUpCheck/checkSignUpOpen" });
+		} else {
+			let error = "";
+			if (isEmailValid(email)) {
+				error = isEmailValid(email);
+			} else {
+				error = isPasswordValid(password);
+			}
+			yield put({
+				type: "signUpCheck/checkSignUpFailure",
+				payload: error,
+			});
+		}
 	} catch (error) {
-		put(checkSignUpFailure(error.toString()));
+		if (error instanceof FirebaseError) {
+			let customizedError = error.message.toString();
+			if (error.code == "auth/email-already-in-use") {
+				customizedError = "This email is already in use, login instead!";
+			} else if (error.code == "auth/invalid-email") {
+				customizedError = "Invalid email format, please try with valid one!";
+			}
+			yield put({
+				type: "signUpCheck/checkSignUpFailure",
+				payload: customizedError,
+			});
+		} else {
+			yield put({
+				type: "signUpCheck/checkSignUpFailure",
+				payload: "Something went wrong, please try again!",
+			});
+		}
+	} finally {
+		yield put({ type: "signUpCheck/checkSignUpSuccess" });
 	}
 }
 
 function* singUpSaga() {
-	yield takeEvery("signUpCheck/checkSingUp", workCheckSignUP);
+	yield takeEvery("signUpCheck/checkSignUp", workCheckSignUP);
 }
 
 export default singUpSaga;
