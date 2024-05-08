914b832cc7160ebd2e93fab277c06ad7e2c94014
sgc93 <smachewgedefaw@gmail.com>
2024-05-07 16:26:34 -0900

    create component to list down top tracks

---
+++ src/features/categories/Track.jsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+const TrackBox = styled.div`
+	padding: 0.5rem 1rem;
+`;
+
+const Track = () => {
+	return <TrackBox>Home Track</TrackBox>;
+};
+
+export default Track;
