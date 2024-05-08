dce19de76df984aced92d7034167c0e56c6310a9
sgc93 <smachewgedefaw@gmail.com>
2024-05-07 16:26:12 -0900

    create component to list down top albums

---
+++ src/features/categories/Album.jsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+const AlbumBox = styled.div`
+	padding: 0.5rem 1rem;
+`;
+
+const Album = () => {
+	return <AlbumBox>Home Album</AlbumBox>;
+};
+
+export default Album;
