4d68aae925e196a5d5b511a3201c7e3814fd73c7
sgc93 <smachewgedefaw@gmail.com>
2024-05-08 11:08:16 -0900

    handle signing in with google

--- src/features/auth/signUp/signUpSaga.js
+++ src/features/auth/signUp/signUpSaga.js
@@ -1,35 +1,47 @@
 import { FirebaseError } from "firebase/app";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+
 import { put, takeEvery } from "redux-saga/effects";
-import { auth } from "../../../config/firebase_config";
+import { auth, googleProvider } from "../../../config/firebase_config";
 import { isEmailValid, isPasswordValid } from "../../../utils/validators";
 
 function* workCheckSignUP(action) {
-	const { email, password } = action.payload;
+	const { email, password, isWithGoogle } = action.payload;
 	try {
-		if (!isEmailValid(email) && !isPasswordValid(password)) {
-			yield createUserWithEmailAndPassword(auth, email, password);
+		if (isWithGoogle) {
+			yield signInWithPopup(auth, googleProvider);
 			yield { type: "signUpCheck/checkSignUpSuccess" };
 			yield put({ type: "signUpCheck/checkSignUpOpen" });
 		} else {
-			let error = "";
-			if (isEmailValid(email)) {
-				error = isEmailValid(email);
+			if (!isEmailValid(email) && !isPasswordValid(password)) {
+				yield createUserWithEmailAndPassword(auth, email, password);
+				yield { type: "signUpCheck/checkSignUpSuccess" };
+				yield put({ type: "signUpCheck/checkSignUpOpen" });
 			} else {
-				error = isPasswordValid(password);
+				let error = "";
+				if (isEmailValid(email)) {
+					error = isEmailValid(email);
+				} else {
+					error = isPasswordValid(password);
+				}
+				yield put({
+					type: "signUpCheck/checkSignUpFailure",
+					payload: error,
+				});
 			}
-			yield put({
-				type: "signUpCheck/checkSignUpFailure",
-				payload: error,
-			});
 		}
 	} catch (error) {
 		if (error instanceof FirebaseError) {
+			console.log(error.log);
 			let customizedError = error.message.toString();
 			if (error.code == "auth/email-already-in-use") {
 				customizedError = "This email is already in use, login instead!";
 			} else if (error.code == "auth/invalid-email") {
 				customizedError = "Invalid email format, please try with valid one!";
+			} else if (error.code == "auth/popup-closed-by-user") {
+				customizedError = "You have close the popup, please try again!";
+			} else if (isWithGoogle) {
+				customizedError = "Unable to Sign in with Google, please try again!";
 			}
 			yield put({
 				type: "signUpCheck/checkSignUpFailure",
