64eeb6e18ba5a25f6100c5c0b9927b29d4a729bd
sgc93 <smachewgedefaw@gmail.com>
2024-05-09 02:39:49 -0900

    create music component to display music player

---
+++ src/features/music/Music.jsx
@@ -0,0 +1,42 @@
+import styled from "@emotion/styled";
+import { useRef, useState } from "react";
+import { timeFormatter } from "../../utils/time_formater";
+import MusicLine from "./MuicLine";
+import MusicController from "./MusicController";
+
+const MusicBox = styled.div`
+	display: flex;
+	gap: 0.7rem;
+
+	width: 100%;
+`;
+
+const Music = () => {
+	const [duration, setDuration] = useState(0);
+	const musicRef = useRef();
+
+	const handleLoadStart = (e) => {
+		const src = e.nativeEvent.srcElement.src;
+		const audio = new Audio(src);
+		audio.onloadedmetadata = function () {
+			if (audio.readyState > 0) {
+				setDuration(audio.duration);
+			}
+		};
+	};
+
+	return (
+		<MusicBox>
+			<audio
+				src="./sample.mp3"
+				hidden
+				ref={musicRef}
+				onLoadStart={handleLoadStart}
+			/>
+			<MusicController />
+			<MusicLine currTime={"00:00"} totalTime={timeFormatter(duration)} />
+		</MusicBox>
+	);
+};
+
+export default Music;
