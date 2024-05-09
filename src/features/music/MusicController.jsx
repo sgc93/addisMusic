67a78a970473705f63c96657b02388b7c3474cec
sgc93 <smachewgedefaw@gmail.com>
2024-05-09 04:19:07 -0900

    create and handle volume controller

--- src/features/music/MusicController.jsx
+++ src/features/music/MusicController.jsx
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled";
 import { useState } from "react";
-import { FaVolumeLow } from "react-icons/fa6";
+import { FaVolumeMute } from "react-icons/fa";
+import { FaVolumeHigh, FaVolumeLow } from "react-icons/fa6";
 import {
 	TbPlayerPause,
 	TbPlayerPlay,
@@ -26,8 +27,32 @@ const Flex = styled.div`
 	align-items: center;
 `;
 
+const VolumeBox = styled.div`
+	position: absolute;
+	top: -3rem;
+	left: 0.7rem;
+	display: flex;
+	gap: 0.2rem;
+
+	padding: 0.5rem 1rem;
+	background-color: var(--color-bg-primary);
+	border-radius: 3rem;
+`;
+
+const VolumeInput = styled.input`
+	width: 4.8rem;
+`;
+const VolValue = styled.span`
+	color: var(--color-text-tertiary);
+	font-size: 1rem;
+	width: 1.2rem;
+	padding-left: 0.2rem;
+`;
+
 const MusicController = ({ music }) => {
 	const [isPaused, setIsPaused] = useState(false);
+	const [volume, setVolume] = useState(50);
+	const [isOpened, setIsOpened] = useState(false);
 
 	const handlePlayingMusic = (pause) => {
 		if (pause) {
@@ -39,11 +64,46 @@ const MusicController = ({ music }) => {
 		}
 	};
 
+	const handleChangingVolume = (vol) => {
+		setVolume(vol);
+	};
+
+	const toggleSound = () => {
+		volume > 0 ? setVolume(0) : setVolume(50);
+	};
+
+	const toggleVolumeBox = () => setIsOpened((isOpened) => !isOpened);
+
 	return (
 		<ControllerBox>
-			<IconButton>
-				<FaVolumeLow />
+			<IconButton handleClick={() => toggleVolumeBox()}>
+				{volume == 0 ? (
+					<FaVolumeMute color="var(--color-text-error)" />
+				) : volume < 50 ? (
+					<FaVolumeLow />
+				) : (
+					<FaVolumeHigh />
+				)}
 			</IconButton>
+			{isOpened && (
+				<VolumeBox>
+					<IconButton handleClick={() => toggleSound()}>
+						{volume == 0 ? (
+							<FaVolumeMute color="var(--color-text-error)" />
+						) : (
+							<FaVolumeLow />
+						)}
+					</IconButton>
+					<VolumeInput
+						type="range"
+						min={0}
+						max={100}
+						value={volume}
+						onChange={(e) => handleChangingVolume(e.target.value)}
+					/>
+					<VolValue>{volume}</VolValue>
+				</VolumeBox>
+			)}
 			<Flex>
 				<IconButton>
 					<TbPlayerTrackPrev />
