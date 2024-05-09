2bd4cc4aea9f6e6eb01d8bc095515a1a4262212b
sgc93 <smachewgedefaw@gmail.com>
2024-05-09 10:11:21 -0900

    handle overview hero in separate file

--- src/features/overview/Overview.jsx
+++ src/features/overview/Overview.jsx
@@ -1,53 +1,20 @@
 import styled from "@emotion/styled";
+import Around from "./Around";
+import BillBoard from "./BilBoard";
 
 const OverviewBox = styled.div`
-	padding: 1rem;
-`;
-
-const BilBoard = styled.div`
-	width: calc(100 - 2rem);
-	height: calc(50% - 2rem);
-
-	border: 2px solid var(--color-border-primary);
-	border-radius: 0.6rem;
-`;
-
-const HeroText = styled.div`
-	padding: 3rem 4rem;
-
 	display: flex;
 	flex-direction: column;
-`;
-const HeroTitle = styled.span`
-	font-size: 2.7rem;
-	font-weight: bold;
-	color: var(--color-bg-primary);
-`;
-const HeroSubtitle = styled.span`
-	font-size: 1.4rem;
-	font-weight: bold;
-	color: #0009;
-`;
+	gap: 0.6rem;
 
-const style = {
-	backgroundColor: "var(--color-text-tertiary)",
-	backgroundImage: 'url("./note2.jpg")',
-	backgroundPosition: "center",
-	backgroundSize: "cover",
-};
+	padding: 1rem;
+`;
 
 const Overview = () => {
 	return (
 		<OverviewBox>
-			<BilBoard style={style}>
-				<HeroText>
-					<HeroTitle>Real message from real feeling!</HeroTitle>
-					<HeroSubtitle>
-						Explore astounding songs, well managed information and enjoy by a
-						sound of mature lyrics.
-					</HeroSubtitle>
-				</HeroText>
-			</BilBoard>
+			<BillBoard />
+			<Around />
 		</OverviewBox>
 	);
 };
