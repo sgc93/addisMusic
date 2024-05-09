181ba7e0ff07d6b91a1d8edf54aaa22f992132a2
sgc93 <smachewgedefaw@gmail.com>
2024-05-09 07:12:17 -0900

    add music slice

--- src/redux/redux_store.js
+++ src/redux/redux_store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import signUpReducer from "../features/auth/signUp/signUpSlice";
+import currMusicReducer from "../features/music/musicSlice";
 
 export const saga = createSagaMiddleware();
 
 export const store = configureStore({
 	reducer: {
 		signUpCheck: signUpReducer,
+		currMusic: currMusicReducer,
 	},
 	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(saga),
 });
