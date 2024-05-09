3e3ae67bae1d0195d647a06fc14becc05aebb37d
sgc93 <smachewgedefaw@gmail.com>
2024-05-09 10:11:46 -0900

    handle top track notifier card

---
+++ src/features/overview/Around.jsx
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+const AroundBox = styled.div``;
+
+const Around = () => {
+	return <AroundBox>Top Tracks In Your Country</AroundBox>;
+};
+
+export default Around;
