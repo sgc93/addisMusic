513b9ae5dbd5d11a416e99795c4c0261c77db2aa
sgc93 <smachewgedefaw@gmail.com>
2024-05-09 17:29:04 -0900

    add button to the music card

--- src/ui/MusicCard.jsx
+++ src/ui/MusicCard.jsx
@@ -1,10 +1,12 @@
 import styled from "@emotion/styled";
+import { useState } from "react";
 import { BiPlay } from "react-icons/bi";
+import { CgAdd } from "react-icons/cg";
+import { GoHeart } from "react-icons/go";
 
 const Card = styled.div`
 	display: flex;
 	flex-direction: column;
-	align-items: center;
 	gap: 0.6rem;
 
 	width: 16rem;
@@ -24,6 +26,10 @@ const ArtistName = styled.span`
 	font-family: "Caveat", cursive;
 	font-weight: 600;
 	font-size: 1.6rem;
+	padding: 0.5rem;
+	overflow: hidden;
+	white-space: nowrap;
+	text-overflow: ellipsis;
 	color: var(--color-text-secondary);
 `;
 
@@ -33,18 +39,43 @@ const MusicName = styled.span`
 	width: 14rem;
 	font-weight: 100;
 	font-size: 1rem;
-	color: var(--color-text-secondary);
-	text-align: center;
+	padding: 0rem 0.5rem 0.5rem;
+	color: var(--color-text-tertiary);
 	overflow: hidden;
 	white-space: nowrap;
 	text-overflow: ellipsis;
 `;
 
-const PlayBtn = styled.button`
+const BtnBox = styled.div`
 	position: absolute;
 	right: 0;
 	bottom: 0;
 
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: end;
+
+	height: 5.5rem;
+`;
+
+const BtnList = styled.div`
+	display: flex;
+	align-items: center;
+	gap: 1rem;
+
+	padding: 0.3rem 1rem;
+	margin: 0.2rem;
+	border-radius: 0.6rem;
+	background-color: var(--color-bg-tertiary);
+`;
+const BtnText = styled.span`
+	padding: 0.4rem 1rem;
+	border-radius: 2rem;
+	color: var(--color-text-primary);
+	background-color: var(--color-bg-primary);
+`;
+const Btn = styled.button`
 	display: flex;
 	align-items: center;
 	justify-content: center;
@@ -54,29 +85,59 @@ const PlayBtn = styled.button`
 	font-size: 1.5rem;
 	border: none;
 	border-radius: 50%;
-	margin: 0.7rem;
 	padding: 0.3rem;
-	background-color: var(--color-bg-tertiary);
-	color: var(--color-text-primary);
+	background-color: var(--color-rad-center);
+	color: var(--color-bg-primary);
 	cursor: pointer;
 	transition: all 0.4s;
 
 	&:hover {
-		background-color: var(--color-bg-primary);
+		background-color: var(--color-text-primary);
 	}
 `;
 
 const MusicCard = ({ song }) => {
+	const [hint, setHint] = useState("");
+
 	const handlePlay = () => {
+		window.open(song.uri);
+	};
+	const addToFavorite = () => {
+		window.open(song.uri);
+	};
+	const addToSongs = () => {
 		window.open(song.uri);
 	};
 
 	return (
 		<Card>
 			<ImgBox>
-				<PlayBtn onClick={() => handlePlay()}>
-					<BiPlay />
-				</PlayBtn>
+				<BtnBox>
+					{hint && <BtnText>{hint}</BtnText>}
+					<BtnList>
+						<Btn
+							onClick={() => addToFavorite()}
+							onMouseEnter={() => setHint("add to favorites")}
+							onMouseLeave={() => setHint("")}
+						>
+							<GoHeart color="red" />
+						</Btn>
+						<Btn
+							onClick={() => addToSongs()}
+							onMouseEnter={() => setHint("add to you songs")}
+							onMouseLeave={() => setHint("")}
+						>
+							<CgAdd />
+						</Btn>
+						<Btn
+							onClick={() => handlePlay()}
+							onMouseEnter={() => setHint("play")}
+							onMouseLeave={() => setHint("")}
+						>
+							<BiPlay />
+						</Btn>
+					</BtnList>
+				</BtnBox>
 				<img
 					src={song.coverArt}
 					alt="cover-art"
