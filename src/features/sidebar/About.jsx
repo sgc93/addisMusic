7447bd2757681aef5ea6d1743ec671fb50854388
sgc93 <smachewgedefaw@gmail.com>
2024-05-09 18:06:54 -0900

    add about box

---
+++ src/features/sidebar/About.jsx
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled";
+
+const AboutBox = styled.div`
+	height: 6rem;
+
+	background-color: var(--color-tab-bg);
+	border-radius: 0.5rem;
+
+	margin-right: 1rem;
+	padding: 0.5rem 1rem;
+	font-size: 1.2rem;
+`;
+
+const About = () => {
+	return <AboutBox></AboutBox>;
+};
+
+export default About;
