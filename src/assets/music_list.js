a012db315db2cf44d9bcf840008d236a59dbaba3
sgc93 <smachewgedefaw@gmail.com>
2024-05-10 03:37:33 -0900

    update music properites

--- src/assets/music_list.js
+++ src/assets/music_list.js
@@ -4,17 +4,20 @@ export const musicList = [
 		name: "Qimemun",
 		artist: "Natty Man",
 		url: "./qimemun.mp3",
+		coverArt: "./bg1.jpg",
 	},
 	{
 		id: "qwerzxcvtyui32567v",
 		name: "tana dar",
 		artist: "Wondy Mak",
 		url: "./tanadar.mp3",
+		coverArt: "./note1.jpg",
 	},
 	{
 		id: "ldjfsd7876poiuy",
 		name: "Wona Nw Wona",
 		artist: "Zerubabel Molla",
 		url: "./wona.mp3",
+		coverArt: "./note5.jpg",
 	},
 ];
