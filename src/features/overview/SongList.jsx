a841b1c62c9d872a26c36d1b7cda200212a279da
sgc93 <smachewgedefaw@gmail.com>
2024-05-09 15:35:47 -0900

    use data from global state

--- src/features/overview/SongList.jsx
+++ src/features/overview/SongList.jsx
@@ -1,10 +1,8 @@
 import styled from "@emotion/styled";
-import { useEffect, useState } from "react";
-import { SPOTIFY_API } from "../../config/spotify_credintials";
+import { useSelector } from "react-redux";
 import Error from "../../ui/Error";
 import LoaderNote from "../../ui/LoaderNote";
 import MusicCard from "../../ui/MusicCard";
-import { timeFormatter } from "../../utils/time_formater";
 
 const ListBox = styled.div`
 	width: 100%;
@@ -41,57 +39,7 @@ const LoaderBox = styled.div`
 `;
 
 const SongList = () => {
-	const [songs, setSongs] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
-	const [error, setError] = useState("");
-
-	useEffect(() => {
-		const fetchSongs = async () => {
-			try {
-				setIsLoading(true);
-				setError("");
-
-				const url =
-					"https://spotify23.p.rapidapi.com/search/?q=%3CREQUIRED%3E&type=multi&offset=0&limit=10&numberOfTopResults=5";
-				const options = {
-					method: "GET",
-					headers: {
-						"X-RapidAPI-Key": SPOTIFY_API,
-						"X-RapidAPI-Host": "spotify23.p.rapidapi.com",
-					},
-				};
-
-				const response = await fetch(url, options);
-
-				if (response.ok) {
-					const result = await response.json();
-					const tracks = [];
-					for (var i = 0; i < result.tracks.items.length; i++) {
-						const track = result.tracks.items[i];
-						tracks.push({
-							id: track?.data?.id,
-							name: track?.data?.name,
-							artist: track?.data?.artists?.items[0]?.profile?.name,
-							duration: timeFormatter(
-								Math.floor(track?.data?.duration?.totalMilliseconds / 1000)
-							),
-							coverArt: track?.data?.albumOfTrack?.coverArt?.sources[0]?.url,
-							uri: track?.data?.uri,
-						});
-					}
-					setSongs(tracks);
-				} else {
-					setError("Unable to fetch data from the server, try again!");
-				}
-			} catch (error) {
-				setError("You have lost you connection, please try again!");
-			} finally {
-				setIsLoading(false);
-			}
-		};
-
-		fetchSongs();
-	}, []);
+	const { isLoading, error, tracks } = useSelector((state) => state.categories);
 
 	return (
 		<ListBox>
@@ -104,10 +52,10 @@ const SongList = () => {
 						</LoaderBox>
 					))}
 				{error && <Error errorMessage={error} />}
-				{songs &&
+				{tracks &&
 					!isLoading &&
 					!error &&
-					songs.map((song) => <MusicCard key={song?.id} song={song} />)}
+					tracks.map((song) => <MusicCard key={song?.id} song={song} />)}
 			</List>
 		</ListBox>
 	);
