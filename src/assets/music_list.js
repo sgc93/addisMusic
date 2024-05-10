5d1dad17ce98483fd7559d3b157c086a650526c0
sgc93 <smachewgedefaw@gmail.com>
2024-05-10 02:51:24 -0900

    create local music provider

---
+++ src/assets/music_list.js
@@ -0,0 +1,20 @@
+export const musicList = [
+	{
+		id: "qwertyuiop245zxcv",
+		name: "Qimemun",
+		artist: "Natty Man",
+		url: "./qimemun.mp3",
+	},
+	{
+		id: "qwerzxcvtyui32567v",
+		name: "tana dar",
+		artist: "Wondy Mak",
+		url: "./tanadar.mp3",
+	},
+	{
+		id: "ldjfsd7876poiuy",
+		name: "Wona Nw Wona",
+		artist: "Zerubabel Molla",
+		url: "./wona.mp3",
+	},
+];
