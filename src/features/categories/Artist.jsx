ff14a51807293a82d96fcfc2411e5294e79253a3
sgc93 <smachewgedefaw@gmail.com>
2024-05-09 16:56:09 -0900

    handle displaying top 10 artist of spotify

--- src/features/categories/Artist.jsx
+++ src/features/categories/Artist.jsx
@@ -1,11 +1,71 @@
 import styled from "@emotion/styled";
+import { useSelector } from "react-redux";
+import ArtistCard from "../../ui/ArtistCard";
+import Error from "../../ui/Error";
+import LoaderBox from "../../ui/LoaderBox";
+import LoaderNote from "../../ui/LoaderNote";
 
 const ArtistBox = styled.div`
+	display: flex;
+	flex-direction: column;
 	padding: 0.5rem 1rem;
 `;
 
+const ArtistTitle = styled.div`
+	display: flex;
+	align-items: center;
+`;
+
+const ListTitle = styled.span`
+	font-size: 1.3rem;
+	font-weight: bold;
+	color: var(--color-text-primary);
+	padding-bottom: 0.2rem;
+	margin: 1rem;
+	border-bottom: 2px solid var(--color-border-primary);
+`;
+
+const ListSubTitle = styled.span`
+	font-size: 1rem;
+	font-weight: bold;
+	color: var(--color-text-tertiary);
+`;
+
+const List = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	gap: 2rem;
+	padding: 1rem;
+`;
 const Artist = () => {
-	return <ArtistBox>Home Artist</ArtistBox>;
+	const { isLoading, error, artists } = useSelector(
+		(state) => state.categories
+	);
+
+	return (
+		<ArtistBox>
+			<ArtistTitle>
+				<ListTitle>Top 10 Artists</ListTitle>
+				<ListSubTitle>based on Spotify</ListSubTitle>
+			</ArtistTitle>
+
+			<List>
+				{isLoading &&
+					Array.from({ length: 8 }).map((index) => (
+						<LoaderBox key={index}>
+							<LoaderNote loadingMessage={"fetching ..."} />
+						</LoaderBox>
+					))}
+				{error && <Error errorMessage={error} />}
+				{artists &&
+					!isLoading &&
+					!error &&
+					artists.map((artist) => (
+						<ArtistCard key={artist.name} artist={artist} />
+					))}
+			</List>
+		</ArtistBox>
+	);
 };
 
 export default Artist;
