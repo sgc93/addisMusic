d47de2ebaacf5ea5c7a3343bae2adaa1e6419294
sgc93 <smachewgedefaw@gmail.com>
2024-05-09 15:37:10 -0900

    add categories saga as forked in root saga

--- src/redux/redux_saga.js
+++ src/redux/redux_saga.js
@@ -1,8 +1,9 @@
 import { all, fork } from "redux-saga/effects";
 import singUpSaga from "../features/auth/signUp/signUpSaga";
+import categoriesSaga from "../features/categories/categoriesSaga";
 
 function* appSaga() {
-	yield all([fork(singUpSaga)]);
+	yield all([fork(singUpSaga), fork(categoriesSaga)]);
 }
 
 export default appSaga;
