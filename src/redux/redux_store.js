a23247124588dafdb4b8b4082270c323447a1021
sgc93 <smachewgedefaw@gmail.com>
2024-05-09 15:36:53 -0900

    add categories state reducer to the store

--- src/redux/redux_store.js
+++ src/redux/redux_store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import signUpReducer from "../features/auth/signUp/signUpSlice";
+import categoriesReducer from "../features/categories/categoriesSlice";
 import currMusicReducer from "../features/music/musicSlice";
 
 export const saga = createSagaMiddleware();
@@ -9,6 +10,7 @@ export const store = configureStore({
 	reducer: {
 		signUpCheck: signUpReducer,
 		currMusic: currMusicReducer,
+		categories: categoriesReducer,
 	},
 	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(saga),
 });
