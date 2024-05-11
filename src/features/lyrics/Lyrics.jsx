9e84018a86131585bae14bfdc4320fccd759745e
sgc93 <smachewgedefaw@gmail.com>
2024-05-11 02:36:58 -0900

    create component to show lyrics

---
+++ src/features/lyrics/Lyrics.jsx
@@ -0,0 +1,91 @@
+import styled from "@emotion/styled";
+import { useState } from "react";
+import { MdLyrics } from "react-icons/md";
+import AroundWindow from "../around/AroundWindow";
+
+const LyricsBox = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 0.6rem;
+
+	padding: 1rem;
+	border-radius: 0.6rem;
+
+		border-radius: 1rem;
+	}
+`;
+
+const LyricsText = styled.div`
+	align-self: flex-start;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 1rem;
+
+	padding: 1rem 1.4rem;
+	margin: 0rem 1rem;
+	border-radius: 0.6rem;
+	background-color: var(--color-text-primary);
+`;
+
+const LyricsTitle = styled.span`
+	align-self: flex-start;
+	display: flex;
+	align-items: center;
+	gap: 0.6rem;
+	font-size: 1.2rem;
+	font-weight: bold;
+	color: var(--color-bg-primary);
+	opacity: 0.9;
+`;
+
+const LyricsSubtitle = styled.span`
+	font-size: 1.1rem;
+	color: var(--color-bg-primary);
+	text-align: center;
+`;
+
+const LyricsBtn = styled.button`
+	align-self: flex-end;
+	padding: 0.5rem 1rem;
+	font-size: 1rem;
+	font-weight: 600;
+	color: var(--color-bg-primary);
+	background-color: var(--color-bg-secondary);
+	border: none;
+	border-radius: 0.6rem;
+	outline: none;
+	cursor: pointer;
+	transition: all 0.4s;
+	&:hover {
+		color: var(--color-text-primary);
+		background-color: var(--color-bg-primary);
+	}
+`;
+
+const Lyrics = () => {
+	const [isOpened, setIsOpened] = useState(false);
+
+	return (
+		<LyricsBox>
+			<LyricsTitle>
+				<MdLyrics opacity={0.8} />
+				Say Lyrics of Top Tracks
+			</LyricsTitle>
+			{isOpened ? (
+				<AroundWindow />
+			) : (
+				<LyricsText>
+					<img src="./note.gif" alt="" width={100} height={100} />
+					<LyricsSubtitle>
+						Unveil the rhythmic heartbeat of your homeland with our curated
+						selection of top tracks from your country.
+					</LyricsSubtitle>
+					<LyricsBtn onClick={() => setIsOpened(true)}>Explore </LyricsBtn>
+				</LyricsText>
+			)}
+		</LyricsBox>
+	);
+};
+
+export default Lyrics;
