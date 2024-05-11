d6876392a59cc124a2f128a9aa14b4da2d6e3908
sgc93 <smachewgedefaw@gmail.com>
2024-05-11 10:45:02 -0900

    add sign in saga as forked one

--- src/redux/redux_saga.js
+++ src/redux/redux_saga.js
@@ -1,9 +1,10 @@
 import { all, fork } from "redux-saga/effects";
 import singUpSaga from "../features/auth/signUp/signUpSaga";
+import singInSaga from "../features/auth/singIn/signInSaga";
 import categoriesSaga from "../features/categories/categoriesSaga";
 
 function* appSaga() {
-	yield all([fork(singUpSaga), fork(categoriesSaga)]);
+	yield all([fork(singUpSaga), fork(singInSaga), fork(categoriesSaga)]);
 }
 
 export default appSaga;
