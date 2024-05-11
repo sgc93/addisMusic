de6a66bd2c08ec95ef0c1528ec28a3f95a043cbd
sgc93 <smachewgedefaw@gmail.com>
2024-05-11 10:45:23 -0900

    add sign in state to root store

--- src/redux/redux_store.js
+++ src/redux/redux_store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import signUpReducer from "../features/auth/signUp/signUpSlice";
+import signInReducer from "../features/auth/singIn/signInSlice";
 import categoriesReducer from "../features/categories/categoriesSlice";
 import currMusicReducer from "../features/music/musicSlice";
 
@@ -9,6 +10,7 @@ export const saga = createSagaMiddleware();
 export const store = configureStore({
 	reducer: {
 		signUpCheck: signUpReducer,
+		signInCheck: signInReducer,
 		currMusic: currMusicReducer,
 		categories: categoriesReducer,
 	},
