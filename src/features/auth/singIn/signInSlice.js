447a94bd946df30ca9f7d8a5cf628b84c9066985
sgc93 <smachewgedefaw@gmail.com>
2024-05-11 10:41:47 -0900

    create sign in slice to store sign in state

---
+++ src/features/auth/singIn/signInSlice.js
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// initialize state
+const initialState = {
+	isLoading: false,
+	error: null,
+	isSignInShown: false,
+};
+
+// define slice with name, initial state and list of reducers
+export const signInSlice = createSlice({
+	name: "signInCheck",
+	initialState,
+	reducers: {
+		checkSignIn(state) {
+			state.isLoading = true;
+			state.error = null;
+		},
+		checkSignInSuccess(state) {
+			state.isLoading = false;
+		},
+		checkSignInFailure(state, action) {
+			state.isLoading = false;
+			state.error = action.payload;
+		},
+		checkSignInOpen(state) {
+			state.error = "";
+			state.isSignInShown = !state.isSignInShown;
+		},
+	},
+});
+
+// export slice actions
+export const {
+	checkSignIn,
+	checkSignInSuccess,
+	checkSignInFailure,
+	checkSignInOpen,
+} = signInSlice.actions;
+
+// export slice reducer
+export default signInSlice.reducer;
