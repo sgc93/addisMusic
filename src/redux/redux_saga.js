7b569638402b0ec65cd86ac2efd3da8a2f44a974
sgc93 <smachewgedefaw@gmail.com>
2024-05-11 15:15:37 -0900

    add password resetting saga as forked brach saga in root saga

--- src/redux/redux_saga.js
+++ src/redux/redux_saga.js
@@ -1,10 +1,15 @@
 import { all, fork } from "redux-saga/effects";
 import singUpSaga from "../features/auth/signUp/signUpSaga";
-import singInSaga from "../features/auth/singIn/signInSaga";
+import singInSaga, { resetSaga } from "../features/auth/singIn/signInSaga";
 import categoriesSaga from "../features/categories/categoriesSaga";
 
 function* appSaga() {
-	yield all([fork(singUpSaga), fork(singInSaga), fork(categoriesSaga)]);
+	yield all([
+		fork(singUpSaga),
+		fork(singInSaga),
+		fork(resetSaga),
+		fork(categoriesSaga),
+	]);
 }
 
 export default appSaga;
