fa6199a435c6cd99c2ec4edb0c8fd71ab303dfd4
sgc93 <smachewgedefaw@gmail.com>
2024-05-11 20:22:48 -0900

    create a utility fucntion to rename a given file

---
+++ src/utils/file.js
@@ -0,0 +1,18 @@
+// renaming file
+export function renameMusicFile(originalFile, newFilename) {
+	const type = originalFile.type;
+	const reader = new FileReader();
+
+	return new Promise((resolve, reject) => {
+		reader.onload = (event) => {
+			const arrayBuffer = event.target.result;
+			const blob = new Blob([arrayBuffer], { type }); // Create a new Blob with the original data
+
+			const newFile = new File([blob], newFilename, { type });
+			resolve(newFile);
+		};
+
+		reader.onerror = (error) => reject(error);
+		reader.readAsArrayBuffer(originalFile);
+	});
+}
