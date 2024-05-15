1abdb8a73eec8e2c010dbb171e4990264ba55cff
sgc93 <smachewgedefaw@gmail.com>
2024-05-14 16:38:04 -0900

    handling status displayling

--- src/features/around/AroundWindow.jsx
+++ src/features/around/AroundWindow.jsx
@@ -2,13 +2,10 @@ import styled from "@emotion/styled";
 import { useEffect, useState } from "react";
 import { BiCheck, BiCheckbox, BiCheckboxChecked } from "react-icons/bi";
 import { BsThreeDots } from "react-icons/bs";
+import { GrClose } from "react-icons/gr";
 import FetchError from "../../ui/FetchError";
 import LoaderNote from "../../ui/LoaderNote";
-import {
-	getTracks,
-	getUserLoc,
-	reverseGeoCode,
-} from "../../utils/user_location";
+import { getUserLoc, reverseGeoCode } from "../../utils/user_location";
 
 const AroundWindowBox = styled.section`
 	display: flex;
@@ -29,12 +26,21 @@ const Title = styled.span`
 const Status = styled.div`
 	display: flex;
 	align-items: center;
+	justify-content: space-between;
+	color: ${(props) =>
+		props.isCompleted == "completed"
+			? "var(--color-success)"
+			: props.isCompleted == "uncompleted"
+			? "var(--color-text-error)"
+			: "var(--color-bg-primary)"};
+	opacity: ${(props) => (props.isCompleted == "loading" ? 0.8 : 1)};
 	gap: 0.5rem;
+	width: 100%;
 `;
 
 const StatusTitle = styled.span`
 	padding: 0.5rem 1rem 0.5rem 0rem;
-	color: var(--color-bg-primary);
+	// color: var(--color-bg-primary);
 	font-weight: bold;
 	opacity: 0.8;
 `;
@@ -43,9 +49,6 @@ const StatusCondition = styled.span`
 	display: flex;
 	align-items: center;
 	font-size: 25px;
-
-	color: ${(props) =>
-		props.isCompleted ? "var(--color-success)" : "var(--color-bg-tertiary)"};
 `;
 
 const AroundWindow = () => {
@@ -54,52 +57,74 @@ const AroundWindow = () => {
 	const [error, setError] = useState({});
 	const [status, setStatus] = useState([
 		{
-			title: "locating user",
-			isCompleted: false,
+			title: "Locating user",
+			isCompleted: "loading",
 		},
 	]);
 
 	const [statusIndex, setStatusIndex] = useState(0);
 
 	useEffect(() => {
+		let timeoutId;
 		const askUserLoc = async () => {
 			setIsLoading(true);
 			setError("");
 			try {
-				const cords = await getUserLoc();
-				if (cords.lat) {
-					updateStatus(true, "finding user country");
+				const locResponse = await getUserLoc();
+				if (locResponse.hasData) {
+					updateStatus("completed", "finding user country");
+					setUserLoc(locResponse.data);
+				} else {
+					updateStatus("uncompleted", "finding user country");
 				}
-				console.log(cords);
-				setUserLoc(cords);
-				const country = await reverseGeoCode(cords);
-				if (country) {
-					updateStatus(true, `fetching top tracks in ${country}`);
+
+				const response = await reverseGeoCode(locResponse.data);
+				if (response.hasData) {
+					updateStatus("completed", `fetching top tracks in ${response.data}`);
+				} else {
+					updateStatus("uncompleted", `fetching top tracks in your country.`);
 				}
-				const tracks = await getTracks(country);
-				if (tracks.items.length) {
-					updateStatus(true, `done`);
-				}
-				console.log(tracks);
+
+				// const tracks = await getTracks(country);
+				// if (tracks) {
+				// 	updateStatus("completed", `done`);
+				// } else {
+				// 	updateStatus("uncompleted", `operation failed, sorry!`);
+				// }
+				// console.log(tracks);
 			} catch (newError) {
-				console.log(newError);
-				setError((error) => newError);
+				setError(newError.message);
 			} finally {
 				setIsLoading(false);
 			}
 		};
 
 		askUserLoc();
+		return () => clearTimeout(timeoutId);
 	}, []);
 
-	const updateStatus = (completed, nextStatusTitle) => {
-		setStatus((status) =>
-			[...status, (status[statusIndex].isCompleted = completed)].push({
-				title: nextStatusTitle,
-				isCompleted: false,
-			})
-		);
-		setStatusIndex((statusIndex) => statusIndex + 1);
+	const updateStatus = (isCurrStatCompleted, nextStatusTitle) => {
+		setStatus((prevStatus) => {
+			console.log(
+				"status index: " +
+					statusIndex +
+					"\n status: " +
+					prevStatus[statusIndex].title
+			);
+
+			const updatedStatus = prevStatus.map((stat, index) =>
+				index === statusIndex
+					? { ...stat, isCompleted: isCurrStatCompleted }
+					: stat
+			);
+
+			return updatedStatus.length === 0 ||
+				updatedStatus[updatedStatus.length - 1].title !== nextStatusTitle
+				? [...updatedStatus, { title: nextStatusTitle, isCompleted: "loading" }]
+				: updatedStatus;
+		});
+
+		setStatusIndex((index) => index + 1);
 	};
 
 	return (
@@ -107,29 +132,41 @@ const AroundWindow = () => {
 			{isLoading && (
 				<>
 					<LoaderNote width={50} loadingMessage={"loading..."} />
-					<Status>
-						<StatusCondition isCompleted={status[statusIndex].isCompleted}>
-							{status[statusIndex].isCompleted ? <BiCheck /> : <BsThreeDots />}
-						</StatusCondition>
-						<StatusTitle>{status[statusIndex].title} </StatusTitle>
-						<StatusCondition isCompleted={status[statusIndex].isCompleted}>
-							{status[statusIndex].isCompleted ? (
-								<BiCheckboxChecked />
-							) : (
-								<BiCheckbox />
-							)}
-						</StatusCondition>
-					</Status>
+					{status.map((stat, index) => {
+						return (
+							<Status key={index} isCompleted={stat.isCompleted}>
+								<StatusCondition isCompleted={stat.isCompleted}>
+									{stat.isCompleted == "completed" ? (
+										<BiCheck />
+									) : stat.isCompleted == "uncompleted" ? (
+										<GrClose size={18} />
+									) : (
+										<BsThreeDots />
+									)}
+								</StatusCondition>
+								<StatusTitle>{stat.title} </StatusTitle>
+								<StatusCondition>
+									{stat.isCompleted == "completed" ? (
+										<BiCheckboxChecked />
+									) : stat.isCompleted == "uncompleted" ? (
+										<BiCheckbox />
+									) : (
+										<BiCheckbox />
+									)}
+								</StatusCondition>
+							</Status>
+						);
+					})}
 				</>
 			)}
-			{error.message && (
+			{error && (
 				<FetchError
 					error={error.message}
 					detail={error.detail}
 					width={"100%"}
 				/>
 			)}
-			{userLoc && `Your location is lat: ${userLoc.lat}, lng: ${userLoc.lng}`}
+			{/* {userLoc && `Your location is lat: ${userLoc.lat}, lng: ${userLoc.lng}`} */}
 		</AroundWindowBox>
 	);
 };
