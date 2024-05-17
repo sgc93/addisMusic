3e3fc3ae8122aad53931fe2d1b2c2fe1d7c00120
sgc93 <smachewgedefaw@gmail.com>
2024-05-17 04:28:50 -0900

    create and build a reusable playlist cards

--- src/ui/PlaylistCard.jsx
+++ src/ui/PlaylistCard.jsx
@@ -1,9 +1,67 @@
+import styled from "@emotion/styled";
+import { how_many_songs } from "../utils/summarizer";
+
+const PlaylistCardBox = styled.div`
+	position: relative;
+	display: flex;
+	flex-direction: column;
+	gap: 1rem;
+
+	background-color: var(--color-bg-secondary);
+	border-radius: 0.2rem;
+	padding: 1rem 2rem;
+	width: calc(50% - 5rem);
+	cursor: pointer;
+	transition: 0.4s;
+
+	&:hover {
+		background-color: var(--color-bg-tertiary);
+	}
+`;
+
+const PlaylistPart = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+`;
+
+const CardName = styled.span`
+	color: var(--color-bg-primary);
+`;
+
+const PlaylistName = styled.span`
+	font-size: 1.3rem;
+	font-weight: bold;
+	text-transform: capitalize;
+	color: var(--color-text-primary);
+`;
+const PlaylistSongs = styled.span`
+	text-transform: capitalize;
+	color: var(--color-bg-primary);
+`;
+const PlaylistTime = styled.span`
+	opacity: 0.8;
+	color: var(--color-bg-primary);
+`;
+
 const PlaylistCard = ({ playlist }) => {
 	const { name, createdAt, updatedAt, musics } = playlist;
+	const isUpdated = !(createdAt === updatedAt);
 	return (
-		<div>
-			<span>{name}</span>
-		</div>
+		<PlaylistCardBox>
+			<PlaylistPart>
+				<PlaylistName>{name}</PlaylistName>
+				<CardName>playlist</CardName>
+			</PlaylistPart>
+			<PlaylistPart>
+				<PlaylistSongs>{how_many_songs(musics.length)}</PlaylistSongs>
+				{isUpdated ? (
+					<PlaylistTime>Updated {updatedAt}</PlaylistTime>
+				) : (
+					<PlaylistTime>Created {createdAt}</PlaylistTime>
+				)}
+			</PlaylistPart>
+		</PlaylistCardBox>
 	);
 };
 
