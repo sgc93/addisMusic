26eeb73cc2fcf84a1110c3b41da39ba0306e677d
sgc93 <smachewgedefaw@gmail.com>
2024-05-17 09:15:47 -0900

    handle playlist updation time summarizing error

--- src/utils/summarizer.js
+++ src/utils/summarizer.js
@@ -1,7 +1,7 @@
 export const how_many_songs = (songs) => {
 	if (songs == 0) {
 		return "has no songs yet!";
-	} else if (songs > 0) {
+	} else if (songs == 1) {
 		return "1 song";
 	} else {
 		return `${songs} songs`;
