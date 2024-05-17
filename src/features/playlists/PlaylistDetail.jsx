0b047c6c26a97029a6dfc26290e8ce5af69134c3
sgc93 <smachewgedefaw@gmail.com>
2024-05-17 05:10:14 -0900

    create a playlist detail box

--- src/features/playlists/PlaylistDetail.jsx
+++ src/features/playlists/PlaylistDetail.jsx
@@ -1,6 +1,40 @@
-const PlaylistDetail = ({ playlist }) => {
+import styled from "@emotion/styled";
+import { BiArrowBack } from "react-icons/bi";
+import IconButton from "../../ui/IconButton";
+
+const DetailBox = styled.section`
+	display: flex;
+	flex-direction: column;
+	gap: 0.7rem;
+
+	width: 100%;
+	height: 100%;
+`;
+const DetailHeader = styled.div`
+	display: flex;
+	width: 100%;
+	height: 40%;
+`;
+
+const DetailContent = styled.div`
+	width: 100%;
+`;
+
+const PlaylistDetail = ({ playlist, setIsDetailing }) => {
 	const { name, createdAt, updatedAt, musics } = playlist;
-	return <div>{name}</div>;
+
+	const backToPlaylists = () => setIsDetailing(false);
+
+	return (
+		<DetailBox>
+			<DetailHeader>
+				<IconButton handleClick={() => backToPlaylists()}>
+					<BiArrowBack />
+				</IconButton>
+			</DetailHeader>
+			<DetailContent>this is the content of the detail</DetailContent>
+		</DetailBox>
+	);
 };
 
 export default PlaylistDetail;
