ca19a746650f785b85da7f8339d6347ecab9ead3
sgc93 <smachewgedefaw@gmail.com>
2024-05-18 10:11:43 -0900

    create a playlist summerizer function

--- src/utils/summarizer.js
+++ src/utils/summarizer.js
@@ -1,3 +1,5 @@
+import { timeFormatter } from "./time_formater";
+
 export const how_many_songs = (songs) => {
 	if (songs == 0) {
 		return "has no songs yet!";
@@ -6,4 +8,67 @@ export const how_many_songs = (songs) => {
 	} else {
 		return `${songs} songs`;
 	}
+};
+
+export const playlist_summarizer = (playlist) => {
+	const { musics } = playlist;
+	const folders = [];
+	const artists = [];
+	const favorites = [];
+	let total_duration = 0;
+
+	musics.forEach((music) => {
+		// duration
+		total_duration += music.duration;
+
+		// favorite musics
+		if (music.isFavorite) {
+			favorites.push(music);
+		}
+
+		// folder by artist
+		if (folders.length > 0) {
+			let isThereBefore = {};
+			folders.forEach((folder) => {
+				if (folder.artist === music.artist) {
+					isThereBefore = {
+						isIt: true,
+						folder: folder,
+					};
+				}
+			});
+
+			if (isThereBefore.isIt) {
+				const { folder } = isThereBefore;
+				folders[folders.indexOf(folder)].musics.push(music);
+			} else {
+				folders.push({
+					artist: music.artist,
+					musics: [music],
+				});
+				artists.push(music.artist);
+			}
+		} else {
+			folders.push({
+				artist: music.artist,
+				musics: [music],
+			});
+			console.log("<=0");
+		}
+	});
+
+	console.log("is folderabole: " + (folders.length > 0));
+	console.log("total folders: " + folders.length);
+	console.log("total artists: " + folders.length);
+	console.log("total duration: " + timeFormatter(total_duration));
+	console.log("favorited musics: " + favorites);
+	console.log("artists: " + artists);
+
+	return {
+		artists: artists,
+		favorites: favorites,
+		folders: folders,
+		isFolderable: folders.length > 0,
+		totalDuration: timeFormatter(total_duration),
+	};
 };
