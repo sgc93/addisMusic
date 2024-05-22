4f041c1714687b6e62461e7eeec3f9e210b031f6
sgc93 <smachewgedefaw@gmail.com>
2024-05-22 09:54:09 -0900

    build structural composition of local song player

--- src/features/local/Local.jsx
+++ src/features/local/Local.jsx
@@ -1,5 +1,35 @@
+import styled from "@emotion/styled";
+import { useState } from "react";
+import { FormInput } from "../auth/Components";
+
+const LocalBox = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 97%;
+
+	margin: 0rem 0.5rem 1rem 0rem;
+	background: linear-gradient(to top, var(--color-bg-tertiary), #ffffff13 10%);
+`;
+
 const Local = () => {
-	return <div>local music lists</div>;
+	const [files, setFiles] = useState([]);
+
+	const handleSelectingFiles = (event) => {
+		const selectedFiles = event.target.files;
+		console.log(selectedFiles);
+	};
+
+	return (
+		<LocalBox>
+			<FormInput
+				type="file"
+				accept="*.mp3"
+				multiple
+				onChange={handleSelectingFiles}
+			/>
+		</LocalBox>
+	);
 };
 
 export default Local;
