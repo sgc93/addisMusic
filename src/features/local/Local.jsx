aad2103ef4a62422781a8193f437c8dc1eca3c03
sgc93 <smachewgedefaw@gmail.com>
2024-05-22 10:14:11 -0900

    handle reading music(s) files from local storage and make them ready to play

--- src/features/local/Local.jsx
+++ src/features/local/Local.jsx
@@ -1,5 +1,11 @@
 import styled from "@emotion/styled";
 import { useState } from "react";
+import { RiFileMusicFill } from "react-icons/ri";
+import {
+	FormInputLabel,
+	FormPlaceholder,
+	LabelIcon,
+} from "../../styles/styled_components";
 import { FormInput } from "../auth/Components";
 
 const LocalBox = styled.div`
@@ -9,7 +15,7 @@ const LocalBox = styled.div`
 	height: 97%;
 
 	margin: 0rem 0.5rem 1rem 0rem;
-	background: linear-gradient(to top, var(--color-bg-tertiary), #ffffff13 10%);
+	background: linear-gradient(to top, var(--color-rad-outer2), #ffffff13 10%);
 `;
 
 const Local = () => {
@@ -17,17 +23,32 @@ const Local = () => {
 
 	const handleSelectingFiles = (event) => {
 		const selectedFiles = event.target.files;
-		console.log(selectedFiles);
+		if (selectedFiles) {
+			setFiles(selectedFiles);
+		}
 	};
 
 	return (
 		<LocalBox>
-			<FormInput
-				type="file"
-				accept="*.mp3"
-				multiple
-				onChange={handleSelectingFiles}
-			/>
+			<>
+				{files.length > 0 && (
+					<audio src={URL.createObjectURL(files[0])} controls />
+				)}
+				<FormInput
+					type="file"
+					accept="*.mp3"
+					multiple
+					onChange={handleSelectingFiles}
+					id="localInput"
+					hidden
+				/>
+				<FormInputLabel htmlFor="localInput">
+					<FormPlaceholder>Select file(s)</FormPlaceholder>
+					<LabelIcon>
+						<RiFileMusicFill size={20} />
+					</LabelIcon>
+				</FormInputLabel>
+			</>
 		</LocalBox>
 	);
 };
