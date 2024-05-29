2af2b7bf7498f178fee062cbb40bbf91f12c4339
sgc93 <smachewgedefaw@gmail.com>
2024-05-28 23:20:43 -0900

    handle opening local musics

--- src/features/local/Local.jsx
+++ src/features/local/Local.jsx
@@ -1,12 +1,8 @@
 import styled from "@emotion/styled";
 import { useEffect, useState } from "react";
-import { RiFileMusicFill } from "react-icons/ri";
+import { BiFolderOpen } from "react-icons/bi";
+import { IoMdArrowDown } from "react-icons/io";
 import { useDispatch } from "react-redux";
-import {
-	FormInputLabel,
-	FormPlaceholder,
-	LabelIcon,
-} from "../../styles/styled_components";
 import TrackCard from "../../ui/TrackCard";
 import { FormInput } from "../auth/Components";
 import { currentMusicIndex, currentMusicList } from "../music/musicSlice";
@@ -18,16 +14,113 @@ const LocalBox = styled.div`
 	margin: 0rem 0.5rem 1rem 0rem;
 `;
 
+const LocalHeader = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+
+	padding: 1rem;
+`;
+const LocalText = styled.span`
+	font-size: 1.2rem;
+	font-weight: bold;
+
+	color: var(--color-bg-primary);
+`;
+
+const LocalBtn = styled.button`
+	display: flex;
+	align-items: center;
+	gap: 0.5rem;
+
+	color: var(--color-text-primary);
+	background-color: #3c3ccd;
+	border: 1px solid #2b2bbb;
+	border-radius: 0.4rem;
+	padding: 0.3rem 0.6rem;
+	font-size: 1rem;
+	font-weight: bold;
+
+	cursor: pointer;
+	transition: all 0.4s;
+	&:hover {
+		color: white;
+	}
+`;
+
+const InputLabel = styled.label`
+	display: flex;
+	align-items: center;
+	gap: 0.5rem;
+
+	color: var(--color-text-primary);
+	background-color: #2b2bbb;
+	border: 1px solid #2b2bbb;
+	border-radius: 0.4rem;
+	padding: 0.3rem 0.6rem;
+	font-size: 1rem;
+	font-weight: bold;
+
+	cursor: pointer;
+	transition: all 0.4s;
+	&:hover {
+		border-color: var(--color-text-primary);
+	}
+`;
+
+const Placeholder = styled.span`
+	display: flex;
+	align-items: center;
+	gap: 0.3rem;
+`;
+
 const MusicList = styled.div`
 	display: flex;
 	flex-direction: column;
 	flex-wrap: wrap;
+
 	gap: 1rem;
 	padding: 1rem;
 `;
 
-const LocalHeader = styled.div``;
-const LocalBody = styled.div``;
+const EmptyBox = styled.div`
+	align-self: center;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 1rem;
+
+	padding-top: 10rem;
+`;
+
+const EmptyTitle = styled.div`
+	text-align: center;
+	font-size: 1.3rem;
+	font-weight: bold;
+
+	color: var(--color-rad-outer);
+`;
+
+const LocalInputLabel = () => {
+	return (
+		<InputLabel htmlFor="localInput">
+			<Placeholder>
+				<BiFolderOpen size={17} />
+				<span>Open file(s)</span>
+			</Placeholder>
+			<IoMdArrowDown size={20} />
+		</InputLabel>
+	);
+};
+
+const EmptyLocal = () => {
+	return (
+		<EmptyBox>
+			<EmptyTitle>Navigate to you local and select music(s)</EmptyTitle>
+			<LocalInputLabel />
+		</EmptyBox>
+	);
+};
 
 const Local = () => {
 	const [files, setFiles] = useState([]);
@@ -60,17 +153,6 @@ const Local = () => {
 
 	return (
 		<LocalBox>
-			<MusicList>
-				{files.map((songFile, index) => (
-					<TrackCard
-						key={index}
-						song={songFile}
-						index={index}
-						shouldMore
-						shouldMoreAdd
-					/>
-				))}
-			</MusicList>
 			<FormInput
 				type="file"
 				accept="*.mp3"
@@ -79,12 +161,27 @@ const Local = () => {
 				id="localInput"
 				hidden
 			/>
-			<FormInputLabel htmlFor="localInput">
-				<FormPlaceholder>Select file(s)</FormPlaceholder>
-				<LabelIcon>
-					<RiFileMusicFill size={20} />
-				</LabelIcon>
-			</FormInputLabel>
+			{files.length > 0 ? (
+				<>
+					<LocalHeader>
+						<LocalText>Selected Musics</LocalText>
+						<LocalInputLabel />
+					</LocalHeader>
+					<MusicList>
+						{files.map((songFile, index) => (
+							<TrackCard
+								key={index}
+								song={songFile}
+								index={index}
+								shouldMore
+								shouldMoreAdd
+							/>
+						))}
+					</MusicList>
+				</>
+			) : (
+				<EmptyLocal />
+			)}
 		</LocalBox>
 	);
 };
