3d9803408e3ef3d353547cb3fc13e6e3d7e6d2fb
sgc93 <smachewgedefaw@gmail.com>
2024-05-30 00:05:29 -0900

    fork root saga and add playlist adder effect saga

--- src/redux/redux_saga.js
+++ src/redux/redux_saga.js
@@ -2,7 +2,9 @@ import { all, fork } from "redux-saga/effects";
 import singUpSaga from "../features/auth/signUp/signUpSaga";
 import singInSaga, { resetSaga } from "../features/auth/singIn/signInSaga";
 import categoriesSaga from "../features/categories/categoriesSaga";
-import playlistSaga from "../features/playlists/playlistSaga";
+import playlistSaga, {
+	playlistAddSaga,
+} from "../features/playlists/playlistSaga";
 
 function* appSaga() {
 	yield all([
@@ -11,6 +13,7 @@ function* appSaga() {
 		fork(resetSaga),
 		fork(categoriesSaga),
 		fork(playlistSaga),
+		fork(playlistAddSaga),
 	]);
 }
 
