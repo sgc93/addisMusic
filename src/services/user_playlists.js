fd5830334947b8adf91d11993aa86805e3f57160
sgc93 <smachewgedefaw@gmail.com>
2024-05-30 06:33:45 -0900

    handle deleting a playlist and its entire file from storage permanently

--- src/services/user_playlists.js
+++ src/services/user_playlists.js
@@ -1,7 +1,8 @@
 // manipulate user playlists: update, delete and create
 
-import { doc, setDoc } from "firebase/firestore";
-import { firestore } from "../config/firebase_config";
+import { deleteDoc, doc, getDoc, setDoc } from "firebase/firestore";
+import { deleteObject, ref } from "firebase/storage";
+import { firestore, storage } from "../config/firebase_config";
 
 export const addPlaylist = async (name, collectionName, currentPlaylists) => {
 	try {
@@ -24,5 +25,66 @@ export const addPlaylist = async (name, collectionName, currentPlaylists) => {
 		};
 	}
 };
+
+export const deletePlaylist = async (
+	collectionName,
+	playlistName,
+	currentPlaylists
+) => {
+	try {
+		const playlistRef = doc(firestore, collectionName, playlistName);
+		const playlistSnap = await getDoc(playlistRef);
+		if (playlistSnap.exists) {
+			const updatedPlaylists = [];
+			currentPlaylists.forEach((playlist) => {
+				if (playlist.name !== playlistName) {
+					updatedPlaylists.push(playlist);
+				}
+			});
+
+			await deleteDoc(playlistRef);
+			const { musics } = playlistSnap.data();
+			musics.forEach((music) => {
+				const songRef = ref(storage, music.songRef);
+				const coverRef = ref(storage, music.coverRef);
+
+				deleteObject(songRef)
+					.then(() => {
+						//
+					})
+					.catch(() => {
+						// return {
+						// 	status: "error",
+						// 	error: "Unable to delete song file, try again please",
+						// };
+					});
+
+				deleteObject(coverRef)
+					.then(() => {
+						//
+					})
+					.catch(() => {
+						// return {
+						// 	status: "error",
+						// 	error: "Unable to delete cover art file, try again please",
+						// };
+					});
+			});
+
+			console.log(updatedPlaylists);
+			return { status: "ok", updatedPlaylists: updatedPlaylists };
+		} else {
+			return {
+				status: "error",
+				error: `${playlistName} doesn't exist anymore!`,
+			};
+		}
+	} catch (error) {
+		return {
+			status: "error",
+			error: `Unable to delete ${playlistName}, Check you connection`,
+		};
+	}
+};
+
 export const renamePlaylist = async () => {};
-export const deletePlaylist = async () => {};
